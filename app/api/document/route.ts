26589850055cff3ad365cf075e37814b819da2bb
MRAlirad <m.r.alirad75@gmail.com>
2023-11-04 19:53:14 +0330

    creat post document API

---
+++ app/api/document/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import prisma from '@/prisma/client';
+
+const createIssueSchema = z.object({
+	title: z.string().min(1).max(255),
+	description: z.string().min(1),
+	sender: z.string().min(1).max(255),
+	receiver: z.string().min(1).max(255),
+});
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+
+	const validation = createIssueSchema.safeParse(body);
+
+	if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+
+	const { subject, description, sender, receiver } = body;
+
+	const newDocument = await prisma.document.create({
+		data: { subject, description, sender, receiver },
+	});
+
+	return NextResponse.json(newDocument, { status: 201 });
+}
